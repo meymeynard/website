5f6cf84041aaf332182b4f354ea944319f4b330b
meymeynard <meymeynard@gmail.com>
2021-03-08 14:26:46 +0800

    chore: add theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 // See https://tailwindcss.com/docs/configuration for details
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.js"],
   // https://github.com/tailwindlabs/tailwindcss-forms
   plugins: [require("@tailwindcss/forms")],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      gray: colors.coolGray,
+      blue: colors.blue,
+    },
+  },
 };
