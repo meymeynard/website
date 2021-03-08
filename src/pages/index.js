10ef10d1c6daa2f3aad9196456f3d4b6a59f2d9e
meymeynard <meymeynard@gmail.com>
2021-03-08 17:03:07 +0800

    chore: update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,48 @@
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
 
 import Layout from "../components/layout";
-import SEO from "../components/seo";
-import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
 
 function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query IndexQuery {
+      site {
+        siteMetadata {
+          title
+          name
+        }
+      }
+    }
+  `);
+
   return (
     <Layout>
-      <SEO
-        keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
-        title="Home"
-      />
-
-      <section className="text-center">
-        <img
-          alt="Cat and human sitting on a couch"
-          className="block w-1/2 mx-auto mb-8"
-          src={catAndHumanIllustration}
-        />
-
-        <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400">
-          Hey there! Welcome to your first Gatsby site.
+      <section className="pt-20">
+        <h2 className="text-3xl">
+          {data.site.siteMetadata.title} {"I'm "}
+          <span className="text-blue-800 font-bold">
+            {data.site.siteMetadata.name}
+          </span>
+          <img className="h-auto" src="src/images/profile.png" />
         </h2>
-
-        <p className="leading-loose">
-          This is a barebones starter for Gatsby styled using{` `}
-          <a
-            className="font-bold text-gray-900 no-underline"
-            href="https://tailwindcss.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Tailwind CSS
-          </a>
-          , a utility-first CSS framework.
+        <p className="text-xl pt-4">
+          {
+            "I'm a web developer based in Manila with over 5 years of experience."
+          }
+          <br />
+          {
+            "I've worked with different startups as: frontend developer, backend developer, android developer and a little bit of UX research."
+          }
         </p>
+      </section>
+      <section className="text-xl pt-8">
+        <p>{"Some of the technologies that I've used:"}</p>
+        <div className="grid grid-cols-2 pt-4">
+          <div>ReactJS</div>
+          <div>TypeScript</div>
+          <div>Ruby on Rails</div>
+          <div>HTML and CSS</div>
+        </div>
       </section>
     </Layout>
   );
