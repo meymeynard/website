194037c3117343f3e460cf4853c0a29732104bae
meymeynard <meymeynard@gmail.com>
2021-03-09 17:21:52 +0800

    chore: add skills section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHtml5, faJs, faReact } from '@fortawesome/free-brands-svg-icons'
+import ruby from '../images/ruby.svg'
+import typescript from '../images/typescript.svg'
 import Layout from "../components/layout";
 
 function IndexPage() {
@@ -23,7 +26,6 @@ function IndexPage() {
           <span className="text-blue-800 font-bold">
             {data.site.siteMetadata.name}
           </span>
-          <img className="h-auto" src="src/images/profile.png" />
         </h2>
         <p className="text-xl pt-4">
           {
@@ -38,11 +40,34 @@ function IndexPage() {
       <section className="text-xl pt-8">
         <p>{"Some of the technologies that I've used:"}</p>
         <div className="grid grid-cols-2 pt-4">
-          <div>ReactJS</div>
-          <div>TypeScript</div>
-          <div>Ruby on Rails</div>
-          <div>HTML and CSS</div>
+          <div>
+            <FontAwesomeIcon icon={faReact} className="mr-2" />
+            ReactJS
+          </div>
+          <div className="flex items-center">
+            <img src={typescript} className="h-6 w-6 -ml-1 mr-1" />
+              TypeScript
+          </div>
+          <div className="flex items-center">
+            <img src={ruby} className="h-6 w-6 -ml-1 mr-1" />
+            Ruby on Rails
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faHtml5} className="mr-2" />
+            HTML and CSS
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faJs} className="mr-2" />
+              Javascript (ES6+)
+          </div>
         </div>
+      </section>
+      <section className="text-xl pt-8">
+        <p>
+          {
+            "You can reach me through here:"
+          }
+        </p>
       </section>
     </Layout>
   );
